9af2b6c8c88f833a7d8cab2ae4c333729e000942
Dawid <dawid.hum1@gmail.com>
2024-12-11 01:22:59 -0300

    Acrescentando a lógica da movimentação.

--- snake_game.py
+++ snake_game.py
@@ -39,12 +39,46 @@ def jogo():
     sair_do_jogo = False
     fim_jogo = False
 
-x1 = largura / 2
-y1 = altura / 2
+    x1 = largura / 2
+    y1 = altura / 2
 
-x1_troca = 0
-y1_troca = 0
+    x1_troca = 0
+    y1_troca = 0
 
-corpo_cobra = []
-comprimento_cobra = 1
+    corpo_cobra = []
+    comprimento_cobra = 1
 
+    comida_x = round(random.randrange(0, largura - tamanho_do_bloco) / 20.0) * 20.0
+    comida_y = round(random.randrange(0, altura - tamanho_do_bloco) / 20.0 ) * 20.0
+
+    relogio = pygame.time.clock()
+
+    while not sair_do_jogo:
+        while fim_jogo:
+            tela.fill(preto)
+            mensagem("Você perdeu! Pressione 1 para continuar ou 0 para sair.", roxo)
+            sua_pontuacao(comprimento_cobra - 1)
+            pygame.display.update()
+
+            for evento in pygame.event.get():
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_q:
+                        sair_do_jogo = True
+                        fim_jogo = False
+                    if evento.key == pygame.K_c:
+                        jogo()
+            
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    sair_do_jogo = True
+                if evento.type == pygame.KEYDOWN:
+                    if evento.key == pygame.K_LEFT and x1_troca == 0:
+                        
+                        x1_troca = -tamanho_do_bloco
+                        y1_troca = 0
+                    
+                    elif evento.key == pygame.K_RIGHT and x1_troca == 0:
+                        x1_troca = tamanho_do_bloco
+                        y1_troca = 0
+
+                        
